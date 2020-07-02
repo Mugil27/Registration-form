16e43a8669c26436ad79a355c1cc18e0e99251ac
Mugil27 <62883154+Mugil27@users.noreply.github.com>
2020-07-02 17:23:43 +0530

    Updates

---
+++ precache-manifest.c220c4097a0ce0b11f48b99bdf1df313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf775e244d6f2bc2000e45dec4c6052f",
+    "url": "/Mugil27/Registration-form.git/index.html"
+  },
+  {
+    "revision": "3df967ffd990fe3bf606",
+    "url": "/Mugil27/Registration-form.git/static/css/main.41970a6c.chunk.css"
+  },
+  {
+    "revision": "22b0127dbef92c7b89e9",
+    "url": "/Mugil27/Registration-form.git/static/js/2.1d95501d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mugil27/Registration-form.git/static/js/2.1d95501d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df967ffd990fe3bf606",
+    "url": "/Mugil27/Registration-form.git/static/js/main.29804ed0.chunk.js"
+  },
+  {
+    "revision": "b32a4f47571167f73356",
+    "url": "/Mugil27/Registration-form.git/static/js/runtime-main.1d94778d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mugil27/Registration-form.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
